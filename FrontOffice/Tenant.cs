60ed49dfb2d2b77a3d29bdc794b894589f0c8d79
Alejandro Mancebo <Antarebo@gmail.com>
2017-06-11 22:02:54 -0300

    commit

---
+++ FrontOffice/Tenant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FrontOffice
+{
+    public class Tenant
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(myFun);
+            t.Name = "Thread1";
+            t.IsBackground = false;
+            t.Start();
+            Console.WriteLine("Main thread Running");
+            Console.ReadKey();
+        }
+    }
+}
