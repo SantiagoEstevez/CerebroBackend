94979f129129f82a8598ae0ec5df844d812c9b13
Santiago Estevez <Santiago.estevez.@gmail.com>
2017-06-21 20:01:36 -0300

    agregado eventos de token.

--- Cerebro14.Services/Controllers/UsuarioController.cs
+++ Cerebro14.Services/Controllers/UsuarioController.cs
@@ -1,11 +1,14 @@
 ﻿using Cerebro14.DAL;
 using Cerebro14.Model;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Runtime.Serialization.Json;
+using System.Security.Claims;
 using System.Text;
 using System.Web.Http;
 using System.Web.Http.Cors;
@@ -17,20 +20,72 @@ namespace Cerebro14.Services.Controllers
     [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
     public class UsuarioController : ApiController
     {
-        [HttpPost, Route("api/Usuario/Login")]        
+        //-------------------------> Manejo del token de autenticacion. <-------------------------
+        public const string Secret = "856FECBA3B06519C8DDDBC80BB080553"; // your symmetric
+
+        [HttpPost, Route("api/Usuario/Login")]
         public IHttpActionResult LoginBackend(User user)
         {
-            IDALUsuarios dalUsu = new DALUsuario();
-            
-            if (!ModelState.IsValid)
-            {
-                return NotFound();
-            }            
-            
-            //Checkear si existe admin con esa cedula o lo que sea
+            user = new User();
+            user.Password = "asasd";
+            user.Username = "nombre";
 
-            return Json(CiudadHelper.CreateToken24hrs());
+            //return Json(CiudadHelper.CreateToken24hrs());
+            //return Json(GenerateToken("asasaasasasaa"));
+            if (CheckUser(user))
+            {
+                return Json(GenerateToken(user.Username));
+            }
+
+            throw new HttpResponseException(HttpStatusCode.Unauthorized);
         }
+
+        public bool CheckUser(User user)
+        {
+            // should check in the database
+            return true;
+        }
+
+        public static string GenerateToken(string username, int expireMinutes = 20)
+        {
+            var symmetricKey = Convert.FromBase64String(Secret);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var now = DateTime.UtcNow;
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                        {
+                        new Claim(ClaimTypes.Name, username)
+                    }),
+
+                Expires = now.AddMinutes(Convert.ToInt32(expireMinutes)),
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(symmetricKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var stoken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(stoken);
+
+            return token;
+        }
+        //[JwtAuthentication]
+        //-------------------------> Fin Manejo del token de autenticacion. <-------------------------
+
+        //[HttpPost, Route("api/Usuario/Login")]        
+        //public IHttpActionResult LoginBackend(User user)
+        //{
+        //    IDALUsuarios dalUsu = new DALUsuario();
+
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return NotFound();
+        //    }            
+
+        //    //Checkear si existe admin con esa cedula o lo que sea
+
+        //    return Json(CiudadHelper.CreateToken24hrs());
+        //}
 
         // GET api/<controller>/5
         [HttpGet, Route("api/Usuario/{ci}/CiudadLat/{cityLat}/CiudadLon/{cityLon}/")]
